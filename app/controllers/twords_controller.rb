45d4c06bbd82243a9a8247b151916f444996b3cc
M. Simon Borg <msimonborg@gmail.com>
2017-08-15 08:39:41 -0400

    refactor twords controller

--- app/controllers/twords_controller.rb
+++ app/controllers/twords_controller.rb
@@ -1,21 +1,25 @@
 class TwordsController < ApplicationController
   SCREEN_NAME = 'realdonaldtrump'.freeze
 
-  before_action :set_date, only: :index
-
   def index
+    set_date
     @tword = Tword.by_screen_name(SCREEN_NAME).in_date_range(@date_range).recent.first
-    if @tword.blank? || @tword.created_at.beginning_of_day != Date.today.beginning_of_day
+    if @tword.blank?
       twords = Twords.new SCREEN_NAME
-      @tword = Tword.create screen_name: SCREEN_NAME, words: twords.words_forward
+      @tword = Tword.create(
+        screen_name: SCREEN_NAME, words: twords.words_forward, created_at: @date_range.first
+      )
     end
   end
 
   private
 
   def set_date
-    return unless %i[year month day].all? { |date_param| params[date_param].present? }
-    date_string = "#{params[:year]}/#{params[:month]}/#{params[:day]}"
-    @date_range = date_string.to_time.beginning_of_day..date_string.to_time.end_of_day
+    @date_range = if %i[year month day].all? { |date_param| params[date_param].present? }
+      date_string = "#{params[:year]}/#{params[:month]}/#{params[:day]}"
+      date_string.to_time.beginning_of_day..date_string.to_time.end_of_day
+    else
+      Time.now.beginning_of_day..Time.now.end_of_day
+    end
   end
 end
