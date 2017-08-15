cf936e6904a731713868adc4118954e8425338bb
M. Simon Borg <msimonborg@gmail.com>
2017-08-15 08:24:29 -0400

    don't need react, restructure JS

---
+++ app/javascript/twords/index.js
@@ -0,0 +1,15 @@
+import WordCloud from 'wordcloud'
+import $ from 'jquery'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const wordsData = document.getElementById('words_data')
+  const words = JSON.parse(wordsData.getAttribute('data'))
+  WordCloud(document.getElementById('my-canvas'), { 
+    list: words,
+    fontFamily: 'Permanent Marker',
+    fontWeight: 600,
+    gridSize: 1,
+    gridSize: Math.round(16 * $('#my-canvas').width() / 2048),
+    weightFactor: 5
+  } );
+});
