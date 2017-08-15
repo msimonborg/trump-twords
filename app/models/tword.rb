e8af1f7966c45a953ed269a36a6339de53df433d
M. Simon Borg <msimonborg@gmail.com>
2017-08-15 08:44:53 -0400

    add documentation comments

--- app/models/tword.rb
+++ app/models/tword.rb
@@ -2,9 +2,13 @@ class Tword < ApplicationRecord
   validates :words, :screen_name, presence: true
   serialize :words, Array
 
+  # Returns twords filtered by screen_name attribute
   scope :by_screen_name, ->(screen_name) { where screen_name: screen_name }
+
+  # Returns twords in descending order of the date created
   scope :recent, -> { order created_at: :desc }
 
+  # Returns twords that were created within the date range
   def self.in_date_range(date_range)
     return all unless date_range
     where created_at: date_range
