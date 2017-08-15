a018bbc3972d86622a55ec407afe89621bb309a3
M. Simon Borg <msimonborg@gmail.com>
2017-08-15 13:15:39 -0400

    look up different date ranges based on params input

--- app/controllers/twords_controller.rb
+++ app/controllers/twords_controller.rb
@@ -1,20 +1,25 @@
 class TwordsController < ApplicationController
   SCREEN_NAME = 'realdonaldtrump'.freeze
 
+  before_action :set_date_range, only: :index
+
   def index
-    set_date
     @tword = Tword.by_screen_name(SCREEN_NAME).in_date_range(@date_range).recent.first
     if @tword.blank?
-      twords = Twords.new SCREEN_NAME
-      @tword = Tword.create(
-        screen_name: SCREEN_NAME, words: twords.words_forward, created_at: @date_range.first
-      )
+      Twords.config do |c|
+        c.up_to { @date_range.first }
+        twords = Twords.new SCREEN_NAME
+        @tword = Tword.create(
+          screen_name: SCREEN_NAME, words: twords.words_forward, created_at: @date_range.first
+        )
+        c.up_to { Time.now }
+      end
     end
   end
 
   private
 
-  def set_date
+  def set_date_range
     @date_range = if %i[year month day].all? { |date_param| params[date_param].present? }
       date_string = "#{params[:year]}/#{params[:month]}/#{params[:day]}"
       date_string.to_time.beginning_of_day..date_string.to_time.end_of_day
