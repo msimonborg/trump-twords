83c798752888dd85547d65f874e1f57f1d8c9575
M. Simon Borg <msimonborg@gmail.com>
2017-08-18 08:47:49 -0400

    a little more refactoring

--- app/javascript/twords/application.jsx
+++ app/javascript/twords/application.jsx
@@ -19,26 +19,28 @@ class Application extends React.Component {
     }
   }
 
-  render() {
-    const handleChange = (date) => {
-      date = date['_d']
-      console.log(date)
-      let url = this.state.apiUrl + date + '.json'
-      let request = new XMLHttpRequest()
-      request.open("GET", url)
-      request.addEventListener("load", () => {
-        let response = JSON.parse(request.response)
-        console.log(response)
-        this.setState({ words: response.words, date: moment(response.date) })
-      });
-      request.send()
-    }
+  handleDateChange(date) {
+    date = date['_d']
+    console.log(date)
+    let url = this.state.apiUrl + date + '.json'
+    let request = new XMLHttpRequest()
+    request.open("GET", url)
+    request.addEventListener("load", () => {
+      let response = JSON.parse(request.response)
+      console.log(response)
+      this.setState({ words: response.words, date: moment(response.date) })
+    });
+    request.send()
+  }
 
+  render() {
     if  (this.state.cloudVisible) {
       return (
         <div>
           {<Cloud words={this.state.words} />}
-          <div id="date-picker">{<DatePickerWrapper selected={this.state.date} onChange={handleChange}/>}</div>
+          <div id="date-picker">
+            {<DatePickerWrapper selected={this.state.date} onChange={this.handleDateChange.bind(this)}/>}
+          </div>
         </div>
       )
     } else {
