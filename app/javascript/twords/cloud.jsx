a4a16743ed5779992051ad2c462222c79985c99f
M. Simon Borg <msimonborg@gmail.com>
2017-08-17 23:15:22 -0400

    add background color

--- app/javascript/twords/cloud.jsx
+++ app/javascript/twords/cloud.jsx
@@ -11,7 +11,8 @@ export default class Cloud extends React.Component {
       fontWeight: 100,
       gridSize: Math.round(16 * $('#my-canvas').width() / 10000),
       weightFactor: 10,
-      minSize: 1
+      minSize: 1,      
+      backgroundColor: 'rgba(224, 239, 237, 0.99)',
     })
 
     return null
